005784db882905eae48e0e0960902b78eb1c2c3c
DESKTOP-98QJDF1\gabri <gabrielao.developer@gmail.com>
2024-05-29 16:24:22 -0300

    finalizando aula 25. Delegates e expressões lambda

--- DELEGATES/DELEGATES/Program.cs
+++ DELEGATES/DELEGATES/Program.cs
@@ -1,16 +1,9 @@
-﻿using System;
-
-namespace DELEGATES
+﻿namespace DELEGATES
 {
     class Program
     {
         public delegate int MetodoDelegate(string nome);
-
-        //public static int Qualquer(string pessoa)
-        //{
-        //    Console.WriteLine("Seu nome: " + pessoa);
-        //    return pessoa.Length;
-        //}
+        //public delegate void MetodoLambda(int idade);
 
         public static void UtilizaDelegate(MetodoDelegate metodoDelegate)
         {
@@ -21,18 +14,46 @@ namespace DELEGATES
 
         static void Main(string[] args)
         {
-            UtilizaDelegate(delegate (string n) {
+
+            #region ANONIMOS
+            UtilizaDelegate(delegate (string n)
+            {
                 Console.WriteLine(" Nome: " + n);
                 return n.Length;
             });
 
-            MetodoDelegate metodoDelegate = delegate (string n) { 
-                Console.WriteLine(" Nome: " + n); 
-                return n.Length; 
+            MetodoDelegate metodoDelegate = delegate (string n)
+            {
+                Console.WriteLine(" Nome: " + n);
+                return n.Length;
             };
 
             var letras = metodoDelegate("madalena");
             Console.WriteLine("Quatidade de letras: " + letras);
+            #endregion
+
+            #region LAMBDA
+
+            UtilizaDelegate((string n) =>
+            {
+                Console.WriteLine(" Nome: " + n);
+                return n.Length;
+            });
+
+            //MetodoDelegate metodoLambda = (n) => { return int.Parse(n); };
+            //metodoLambda("joseroberto");
+
+            MetodoDelegate metodoDelegate2 = (string n) =>
+            {
+                Console.WriteLine(" Nome: " + n);
+                return n.Length;
+            };
+
+            var letras2 = metodoDelegate2("GABRIEL");
+            Console.WriteLine("Quatidade de letras: " + letras2);
+
+
+            #endregion
 
             Console.WriteLine("fim delegate");
             Console.ReadKey();
